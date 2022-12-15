ccbfe27d51d8707e8e49e2d24ecbf0cbc7e6941e
goldilock-zone <sarbajitghosh2002@gmail.com>
2022-12-15 17:53:18 +0530

    Initial design of Trend Model

---
+++ Models/TrendModel.py
@@ -0,0 +1,24 @@
+import math
+import numpy as np
+import pandas as pd
+
+class TrendModel:
+    def __init__():
+        pass
+
+    def getTrainingPeriod():
+        pass
+
+    def analyze():
+        """
+        Functions: Analyzes data for one security
+        """
+        pass
+
+    def run() -> pd.DataFrame:
+        """
+        Functions: Use the analyze function above to get opportunity list
+        """
+
+if __name__ == '__main__':
+    pass
