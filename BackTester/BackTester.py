d1b03316ee2174fd1964b3e988da16ac30aef5b7
goldilock-zone <sarbajitghosh2002@gmail.com>
2022-12-15 17:52:16 +0530

    Initial Design of Backtester components

---
+++ BackTester/BackTester.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+class BackTester:
+    def __init__(self, 
+        StockDataDict:dict, 
+        list_stock,
+        initial_capital,
+        max_training_period,
+        ):
+        pass
---
+++ BackTester/Broker.py
@@ -0,0 +1,17 @@
+import pandas as pd
+class Broker:
+    """
+    Functions: This mainly takes care of transaction cost and makes an order log
+    """
+    def __init__(self,*, current_data, buffer_percent, transaction_cost_percent, order_fill_time = 'Typical Price'):
+        pass
+
+    def buy(self,*, ticker, amount, existingOrderFlag):
+        pass
+
+    def sell(self,*, ticker, amount, existingOrderFlag):
+        pass
+
+    def order_log(self):
+        out_log = ""
+        return out_log
