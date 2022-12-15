8170f164e2086a464949f4ef392531665fff991e
goldilock-zone <sarbajitghosh2002@gmail.com>
2022-12-15 17:23:47 +0530

    designing portfolio manager

---
+++ Managers/PortfolioManager.py
@@ -0,0 +1,29 @@
+import math
+import numpy as np
+import pandas as pd
+
+class PortfolioManager:
+    
+    def __init__(self):
+        self.model_dictionary = {}
+        self.cash = 0
+        self.current_account_size = 0
+        self.n_rebal_day = 0
+        self.rebal_period = 30
+        
+    #setup and output
+    def dataRequest(self):
+        pass
+
+    def loggingOutput(self):
+        pass
+
+    #functions
+    def time_update(self):
+        pass
+    
+    def rebalance(self):
+        pass
+
+    def cashManager(self):
+        pass
