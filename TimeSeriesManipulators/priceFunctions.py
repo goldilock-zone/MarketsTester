55eb864b20bb13a3f47f348460e76ed14ec20e18
goldilock-zone <sarbajitghosh2002@gmail.com>
2022-12-16 12:37:37 +0530

    Testing Out Price Functions

--- TimeSeriesManipulators/priceFunctions.py
+++ TimeSeriesManipulators/priceFunctions.py
@@ -1,77 +1,245 @@
 import math
 import talib as TA
 import scipy 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as scst
 
 class priceFunctions:
+    OPEN = 'Open'
+    HIGH = 'High'
+    LOW = 'Low'
+    CLOSE = 'Close'
+    VOLUME = 'Volume'
 
-    def __init__(self):
-        pass
+    def __init__(self, input_df, training_period, fill_time):
 
-    #Cleaning and optimzations
+        self.training_period = training_period
+        self.input_df = input_df[-training_period:]
+
+        self.open_array = np.array(self.input_df[self.OPEN])
+        self.high_array = np.array(self.input_df[self.HIGH])
+        self.low_array = np.array(self.input_df[self.LOW])
+        self.close_array = np.array(self.input_df[self.CLOSE])
+        self.volume_array = np.array(self.input_df[self.VOLUME])
+        
+        self.fill_time = fill_time
+
+        #cache
+        self.typprice = []
+        self.avgprice = []
+
+    #Housekeeping
     def validation(self):
         pass
 
     def parameterIterator(self):
         pass
 
+    def graphing(self, list_of_arrays: list):
+        """ 
+        Format of input:
+        list of arrays = [[header1, array1], [header2, array2], ......]
+        """
+
+        fig, ax_tuple = plt.subplots(len(list_of_arrays), 1)
+
+        for index, ax in enumerate(ax_tuple):
+            ax.plot(list_of_arrays[index][1])
+            ax.set_xlabel('Time')
+            ax.set_ylabel(list_of_arrays[index][0])
+
+        plt.show()
+
+    #Price Transforms
+    def typical_price(self):
+        if self.typprice == []:
+            self.typprice = TA.TYPPRICE(self.high_array, self.low_array, self.close_array)
+        return self.typprice
+
+    def avgprice(self):
+        if self.avgprice == []:
+            self.avgprice = TA.AVGPRICE(self.open_array, self.high_array, self.low_array, self.close_array)
+        return self.avgprice
+
     #Trend functions
-    def EMA(self, graphing = False):
+    def EMA(self, lookback, input_array = [], graphing = False):
         #Exponential Moving Average
-        
-        pass
+        if input_array == []:
+            input_array = self.typical_price()
+
+        ema = TA.EMA(input_array, timeperiod = lookback)
+
+        if graphing:
+            self.graphing([
+                ['EMA', ema],
+                ['Price', input_array]
+                ])
+
+        return ema
     
-    def SMA(self, graphing = False):
+    def SMA(self, lookback, input_array = [], graphing = False):
         #Simple Moving Average
-        pass
+        if input_array == []:
+            input_array = self.typical_price()
 
-    def CCI(self, graphing = False):
+        sma = TA.MA(input_array, timeperiod = lookback)
+
+        if graphing:
+            self.graphing([
+                ['SMA', sma],
+                ['Price', input_array]
+                ])
+                
+        return sma
+
+    def CCI(self, lookback, graphing = False):
         #Commodity Channel Index
-        pass
+        cci = TA.CCI(self.high_array, self.low_array, self.close_array, timeperiod = lookback)
 
+        if graphing:
+            self.graphing([
+                ['CCI', cci],
+                ['Price', self.typical_price()]
+                ])
+                
+        return cci
+        
     #Volume functions
     def OBV(self, graphing = False):
         #On Balance Volume
-        pass
+        obv = TA.OBV(self.close_array, self.volume_array)
 
-    def PPO(self, graphing = False):
+        if graphing:
+            self.graphing([
+                ['OBV', obv],
+                ['Price', self.typical_price()]
+                ])
+                
+        return obv
+
+    def PPO(self, fastperiod, slowperiod, graphing = False):
         #Percentage Price Oscillator
-        pass
+        
+        PPO = TA.PPO(self.close_array, fastperiod = fastperiod, slowperiod=slowperiod, matype=0)
+
+        if graphing:
+            self.graphing([
+                ['PPO', PPO],
+                ['Price', self.typical_price()]
+                ])
+                
+        return PPO
     
     #Volatility functions
-    def NATR(self, graphing = False):
+    def NATR(self, lookback, graphing = False):
         #Normalized Average True Range
-        pass
+        natr = TA.NATR(self.high_array, self.low_array, self.close_array, timeperiod=lookback)
+        if graphing:
+            self.graphing([
+                ['NATR', natr],
+                ['Price', self.typical_price()]
+                ])
+        
+        return natr
+
+    def ROC(self, lookback, graphing = False):
+        #Rate of change
+        roc = TA.ROC(self.close_array, timeperiod=lookback)
+        if graphing:
+            self.graphing([
+                ['ROC', roc],
+                ['Price', self.typical_price()]
+                ])
+                
+        return roc
 
     #Statistical functions
     def returnDistribution(self, graphing = False):
         pass
     
-    def correlation(self, graphing = False):
-        pass
+    def correlation_HL(self, lookback, graphing = False):
+        corr = TA.CORREL(self.high_array, self.low_array, timeperiod=lookback)
+        if graphing:
+            self.graphing([
+                ['HL_Corr', corr],
+                ['Price', self.typical_price()]
+                ])
+                
+        return corr
 
-    def linregAngle(self, graphing = False):
-        pass
 
-    def linregSlope(self, graphing = False):
-        pass
+    def linregAngle(self, lookback, graphing = False):
+        lra = TA.LINEARREG_ANGLE(self.close_array, timeperiod=lookback)
+        if graphing:
+            self.graphing([
+                ['LinReg Angle', lra],
+                ['Price', self.typical_price()]
+                ])
+                
+        return lra
 
-    def stddev(self, graphing = False):
-        pass
+    def linregSlope(self, lookback, graphing = False):
+        lrs = TA.LINEARREG_SLOPE(self.close_array, timeperiod=lookback)
+        if graphing:
+            self.graphing([
+                ['LinReg Slope', lrs],
+                ['Price', self.typical_price()]
+                ])
+                
+        return lrs
 
-    def variance(self, graphing = False):
-        pass
+    def stddev(self, lookback, graphing = False):
+        std = TA.STDDEV(self.close_array, timeperiod=lookback, nbdev=1)
+        if graphing:
+            self.graphing([
+                ['STD', std],
+                ['Price', self.typical_price()]
+                ])
+                
+        return std
 
-    def skew(self, graphing = False):
-        #Using scipy
-        pass
+    def variance(self, lookback, graphing = False):
+        var = TA.VAR(self.close_array, timeperiod=lookback, nbdev=1)
+        if graphing:
+            self.graphing([
+                ['VAR', var],
+                ['Price', self.typical_price()]
+                ])
+                
+        return var
 
-    def kurtosis(self, graphing = False):
-        #Using scipy
-        pass
-    
-    #Tests
-    def _tests(self):
-        pass
+    def skew(self, lookback, graphing = False):
+        arr = self.typical_price()
+        out = [0 for i in range(lookback)]
+        for i in range(len(arr) - lookback):
+            temp_arr = arr[i: i + lookback]
+            skew_val = scst.skew(temp_arr)
+            out.append(skew_val)
+        
+        if graphing:
+            self.graphing([
+                ['LagSkew', out],
+                ['Price', self.typical_price()]
+                ])
+                
+        return out
+
+    def kurtosis(self, lookback, graphing = False):
+        arr = self.typical_price()
+        out = [0 for i in range(lookback)]
+        for i in range(len(arr) - lookback):
+            temp_arr = arr[i: i + lookback]
+            skew_val = scst.kurtosis(temp_arr)
+            out.append(skew_val)
+        
+        if graphing:
+            self.graphing([
+                ['LagSkew', out],
+                ['Price', self.typical_price()]
+                ])
+                
+        return out
 
 if __name__ == "__main__":
     pass
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import talib as TA
 import numpy as np
-from talib import MA_Type
+from DataGetter.DataGetter import BasketStockData_Backtest
+from TimeSeriesManipulators.priceFunctions import priceFunctions
 
-close = np.random.random(100)
-output = TA.BBANDS(close, matype=MA_Type.T3)
+stock_data = BasketStockData_Backtest()
+x = stock_data.generate_dict(start="2020-01-01", end = "2021-01-01", list_of_tickers = ['MSFT', 'GOOG'], update_data=True)
+    
+funcs = priceFunctions(input_df = x['MSFT'], training_period = 250, fill_time = 'Open')
+# funcs.ROC(lookback = 6, graphing = True)
+# funcs.correlation_HL(lookback = 6, graphing = True)
+# funcs.linregAngle(lookback = 6, graphing = True)
+# funcs.linregSlope(lookback = 6, graphing = True)
+# funcs.stddev(lookback = 6, graphing = True)
+# funcs.variance(lookback = 6, graphing = True)
 
-print(output)
+funcs.kurtosis(lookback = 20, graphing = True)
